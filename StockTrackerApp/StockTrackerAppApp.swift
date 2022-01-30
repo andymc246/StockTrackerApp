45ba5b1e77d2879234ff802ea5b0af8088c08dcc
andymc246 <andymc246@gmail.com>
2022-01-29 23:24:12 -0500

    Initial Commit

---
+++ StockTrackerApp/StockTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockTrackerAppApp.swift
+//  StockTrackerApp
+//
+//  Created by Andy Mac on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct StockTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
