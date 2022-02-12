88003f5cee9807da743c11eecb2bdcf8166b9af8
andymc246 <andymc246@gmail.com>
2022-02-11 22:32:09 -0500

    Firebase Files

--- StockTrackerApp/AppFeatures/StockQuotes/Managers/MockStockQuoteManager.swift
+++ StockTrackerApp/AppFeatures/StockQuotes/Managers/MockStockQuoteManager.swift
@@ -14,7 +14,7 @@ final class MockStockQuoteManager: QuoteManager, ObservableObject {
     
     func download(stocks: [String], completion: @escaping (Result<[Quote], NetworkError>) -> Void) {
         let aapl = Quote (symbol: "AAPL", open: "162.45", high: "163.84", low: "158.59", price: "158.96", change: "-0.40", changePercent: "-0.40%")
-        let tsla = Quote (symbol: "TSLA", open: "933.36", high: "935.39", low: "844.14", price: "844.56", change: "-92.53", changePercent: "-9.92%")
+        let tsla = Quote (symbol: "TSLA", open: "935.00", high: "946.27", low: "920.00", price: "931.22", change: "+8.89", changePercent: "0.98%")
         
         //creating a for loop that runs through the parameters of each stock quotes in this mock file
         for _ in 0..<8 {
